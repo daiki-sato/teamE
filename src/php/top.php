07473e76807415b3d7ec4443cc3598fbfe281f99
【daiki】 <daiki20010705@icloud.com>
2021-04-30 19:58:48 +0900

    headerの大枠完成

--- src/php/top.php
+++ src/php/top.php
@@ -10,6 +10,21 @@
   <link rel="stylesheet" href="../css/top.css">
 </head>
 <body>
-  <?php include($_SERVER['DOCUMENT_ROOT']."/_header.php"); ?>
+  <?php 
+  include("./_header.php"); 
+  ?>
+
+
+  <section class="category-search">
+
+  <div class="search-area">
+    <h2 class="search-area__title">就活サービスから探す</h2>
+  </div>
+  <ul class="category-search__menu">
+    <li><a class="category-search__menu__list" href="">エージェント</a></li>
+    <li><a class="category-search__menu__list" href="">イベント</a></li>
+    <li><a class="category-search__menu__list" href="">サービス</a></li>
+  </ul>
+</section>
 </body>
 </html>
