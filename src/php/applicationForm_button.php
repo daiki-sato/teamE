78b3b2390b01d6b319605d22bdc130bf77b5b1ec
Hina Takahashi <hina0414takahashi@gmail.com>
2021-05-24 13:47:59 +0900

    applicationForm_button

---
+++ src/php/applicationForm_button.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>applicationForm_button</title>
+  <link rel="stylesheet" href="../css/reset.css">
+  <link rel="stylesheet" href="../css/applicationForm_button.css">
+</head>
+<body>
+  <div class="applicationForm-buttons">
+  
+    <div class="applicationForm-submitbutton">
+      <p>
+        <span>次へ</span>
+      </p>
+    </div>
+
+    <div class="applicationForm-cancelbutton">
+      <p>
+        <span>申し込みをキャンセルする<br>(入力情報は保存されません)</span>
+      </p>
+    </div>
+    
+  </div>
+</body>
+</html>
