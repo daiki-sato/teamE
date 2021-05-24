c791ace3b661d9875a735f7fc14cbca92b9f7842
Hina Takahashi <hina0414takahashi@gmail.com>
2021-05-24 11:50:26 +0900

    top sp card

---
+++ src/php/top_sp_card.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>top_sp_card</title>
+  <link rel="stylesheet" href="../css/reset.css">
+  <link rel="stylesheet" href="../css/top_sp_card.css">
+</head>
+<body>
+<div class="applicationForm-compare__contents">
+        <div class="applicationForm-compare__contents-summary">
+  
+          <div class="applicationForm-compare__contents-summary--left">
+            <img src="#" class="applicationForm-compare__image" alt="エージェント画像が入ります">
+          </div>
+  
+          <div class="applicationForm-compare__contents-summary--right">    
+            <p class="applicationForm-compare__agant-name--box">
+              <a href="#" class="applicationForm-compare__agant-name">キャリセン</a>
+            </p>
+            <p class="applicationForm-compare__hashtag">
+              #星3以上
+            </p>
+            <p class="applicationForm-compare__stars">
+              <span class="applicationForm-compare__stars-rating" data-rate="3"></span>&ensp;3.0
+            </p>
+          </div>
+  
+        </div>
+  
+        <div class="applicationForm-compare__contents-explanation">
+          採用コンサルタントが、じっくり1時間の個別面談。<br>
+          一人一人に合った納得内定獲得を徹底的にサポート。
+        </div>
+  
+        <div class="applicationForm-compare-buttons">
+          <p>
+            <span class="applicationForm-compare-button__article">解説記事</span>
+          </p>
+          <p id="submitbutton" onClick='showDialog()'>
+            <span class="applicationForm-compare-button__submit">お申し込みリストに<br>追加する</span>
+          </p>  
+        </div>
+      </div>
+</body>
+</html>
