dd6ab3e979f190f1dd6a02204d8d249c14572e9e
Hina Takahashi <hina0414takahashi@gmail.com>
2021-05-24 13:03:27 +0900

    よく比較・検討されているエージェント-sp

---
+++ src/php/applicationForm_compare.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>applicationForm_compare</title>
+  <link rel="stylesheet" href="../css/reset.css">
+  <link rel="stylesheet" href="../css/applicationForm_compare.css">
+</head>
+<body>
+
+      <div class="applicationForm-compare__title">
+          <h4 class="applicationForm-compare__text">よく比較・検討されているエージェント</h4>
+          <h5 class="applicationForm-compare__text2">内定者は平均5エージェントから話を聞いています！</h5>
+      </div>
+  
+      <div class="applicationForm-compare__contents">
+        <div class="applicationForm-compare__contents-summary">
+  
+          <div class="applicationForm-compare__contents-summary--left">
+            <img src="#" class="applicationForm-compare__image" alt="エージェント画像が入ります">
+          </div>
+  
+          <div class="applicationForm-compare__contents-summary--right">    
+            <p class="applicationForm-compare__agant-name--box">
+              <a href="#" class="applicationForm-compare__agant-name">キャリセン</a>
+            </p>
+            <p class="applicationForm-compare__hashtag">
+              #星3以上
+            </p>
+            <p class="applicationForm-compare__stars">
+              <span class="applicationForm-compare__stars-rating" data-rate="3"></span>&ensp;3.0
+            </p>
+          </div>
+  
+        </div>
+  
+        <div class="applicationForm-compare__contents-explanation">
+          採用コンサルタントが、じっくり1時間の個別面談。<br>
+          一人一人に合った納得内定獲得を徹底的にサポート。
+        </div>
+  
+        <div class="applicationForm-compare-buttons">
+          <p>
+            <span class="applicationForm-compare-button__article">解説記事</span>
+          </p>
+          <p id="submitbutton" onClick='showDialog()'>
+            <span class="applicationForm-compare-button__submit">お申し込みリストに<br>追加する</span>
+          </p>  
+        </div>
+     </div>
+
+</body>
+</html>
