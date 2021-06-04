f93ab6f2f0d1ec6b43d5e62a7abcb7e227e7908d
chiyoko-hayashi <chiyoko.hayashi@harbors.sh>
2021-06-04 22:06:39 +0900

    mysqlのエラーを直した

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -43,8 +43,12 @@ CREATE TABLE agents (
 -- status=TRUE;
 
 INSERT INTO `agents` (`id`, `agent_name`, `image_url`, `overview`, `upper_limit`, `deadline`, `button_type`, `pickup`, `star`, `official_link`, `article_link`, `memo`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'キャリセン', 'https://careecen-shukatsu-agent.com/wp-content/themes/thinktwice/assets/images/logo.png', '株式会社シンクトワイスが運営する就職支援、新卒紹介のエージェントサービス。面接後にフィードバックをもらえる！自分に合った企業を見つけることができる！ESの添削、面接練習をすることで自信に繋がる！', NULL, NULL, 1, NULL, 3.5, 'https://careecen-shukatsu-agent.com/ ', 'https://reashu.com/careecen-shukatsu-agent/', NULL, 1, '2021-05-22 04:35:50', '2021-05-22 04:35:50'),
-(2, 'キャリアチケット', 'https://careerticket.jp/img/top_ogp.jpg', '参加企業が厳選されておりブラック企業を避けられる！\r\nエントリーシート（ES）や面接のフィードバックがもらえる！\r\n早めに内定を確保できる！', NULL, NULL, 1, NULL, 4, 'https://careerticket.jp/', 'https://reashu.com/carrer-ticket/', NULL, 1, '2021-05-27 03:35:51', '2021-05-27 03:35:51');
+(1, 'キャリセン', 'https://careecen-shukatsu-agent.com/wp-content/themes/thinktwice/assets/images/logo.png', '株式会社シンクトワイスが運営する就職支援、新卒紹介のエージェントサービス。面接後にフィードバックをもらえる！自分に合った企業を見つけることができる！ESの添削、面接練習をすることで自信に繋がる！', NULL, NULL, 1, NULL, 1, 'https://careecen-shukatsu-agent.com/ ', 'https://reashu.com/careecen-shukatsu-agent/', NULL, 1, '2021-05-22 04:35:50', '2021-05-22 04:35:50'),
+(2, 'キャリアチケット', 'https://careerticket.jp/img/top_ogp.jpg', '参加企業が厳選されておりブラック企業を避けられる！\r\nエントリーシート（ES）や面接のフィードバックがもらえる！\r\n早めに内定を確保できる！', NULL, NULL, 1, NULL, 2, 'https://careerticket.jp/', 'https://reashu.com/carrer-ticket/', NULL, 1, '2021-05-27 03:35:51', '2021-05-27 03:35:51'),
+(3, 'キャリセン2', 'https://careecen-shukatsu-agent.com/wp-content/themes/thinktwice/assets/images/logo.png', '株式会社シンクトワイスが運営する就職支援、新卒紹介のエージェントサービス。面接後にフィードバックをもらえる！自分に合った企業を見つけることができる！ESの添削、面接練習をすることで自信に繋がる！', NULL, NULL, 1, NULL, 3.5, 'https://careecen-shukatsu-agent.com/ ', 'https://reashu.com/careecen-shukatsu-agent/', NULL, 1, '2021-05-22 04:35:50', '2021-05-22 04:35:50'),
+(4, 'キャリアチケット2', 'https://careerticket.jp/img/top_ogp.jpg', '参加企業が厳選されておりブラック企業を避けられる！\r\nエントリーシート（ES）や面接のフィードバックがもらえる！\r\n早めに内定を確保できる！', NULL, NULL, 1, NULL, 4, 'https://careerticket.jp/', 'https://reashu.com/carrer-ticket/', NULL, 1, '2021-05-27 03:35:51', '2021-05-27 03:35:51'),
+(5, 'キャリセン3', 'https://careecen-shukatsu-agent.com/wp-content/themes/thinktwice/assets/images/logo.png', '株式会社シンクトワイスが運営する就職支援、新卒紹介のエージェントサービス。面接後にフィードバックをもらえる！自分に合った企業を見つけることができる！ESの添削、面接練習をすることで自信に繋がる！', NULL, NULL, 1, NULL, 4.5, 'https://careecen-shukatsu-agent.com/ ', 'https://reashu.com/careecen-shukatsu-agent/', NULL, 1, '2021-05-22 04:35:50', '2021-05-22 04:35:50'),
+(6, 'キャリアチケット3', 'https://careerticket.jp/img/top_ogp.jpg', '参加企業が厳選されておりブラック企業を避けられる！\r\nエントリーシート（ES）や面接のフィードバックがもらえる！\r\n早めに内定を確保できる！', NULL, NULL, 1, NULL, 5, 'https://careerticket.jp/', 'https://reashu.com/carrer-ticket/', NULL, 1, '2021-05-27 03:35:51', '2021-05-27 03:35:51');
 
 
 DROP TABLE IF EXISTS agent_tag_table;
@@ -57,7 +61,11 @@ CREATE TABLE agent_tag_table (
 INSERT INTO `agent_tag_table` (`id`, `agent_id`, `tag_id`) VALUES
 (1, 1, 2),
 (2, 1, 1),
-(3, 2, 1);
+(3, 2, 1),
+(4, 3, 3),
+(5, 4, 2),
+(6, 4, 3),
+(7, 6, 3);
 
 DROP TABLE IF EXISTS agent_kw_table;
 CREATE TABLE agent_kw_table (
@@ -78,7 +86,9 @@ CREATE TABLE tag_table (
 
 INSERT INTO `tag_table` (`id`, `tag_name`) VALUES
 (1, '理系'),
-(2, '文系');
+(2, '文系'),
+(3, '星3以上'),
+(4, '星4以上');
 
 DROP TABLE IF EXISTS kw_table;
 CREATE TABLE kw_table (
